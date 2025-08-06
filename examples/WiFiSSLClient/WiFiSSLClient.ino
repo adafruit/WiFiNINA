8b45e4e93dec0ba2b458260d9e4811005f03af79
foamyguy <foamyguy@gmail.com>
2025-08-06 10:20:00 -0500

    dont use twitter URL for SSL client example

--- examples/WiFiSSLClient/WiFiSSLClient.ino
+++ examples/WiFiSSLClient/WiFiSSLClient.ino
@@ -24,8 +24,8 @@ int status = WL_IDLE_STATUS;
 // use the numeric IP instead of the name for the server:
 //IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
 
-#define SERVER "cdn.syndication.twimg.com"
-#define PATH   "/widgets/followbutton/info.json?screen_names=adafruit"
+#define SERVER "httpbin.org"
+#define PATH   "/get"
 
 // Initialize the SSL client library
 // with the IP address and port of the server
